0766839cda18026e3b85279b1f9cb0635ccce55a
Entaro Adun <dcwmark@gmail.com>
2023-09-07 16:13:37 -0400

    daily commits

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -25,26 +25,54 @@ class SketchPad {
 
   #addEventListeners() {
     this.canvas.onmousedown = (evt) => {
-      const rect = this.canvas.getBoundingClientRect();
-      /**
-       * Obtain the mouse co-ordinate of the mouse by
-       * taking the clientX of the event minus
-       *   the left side of the canvas rectangle,
-       *   thereby, getting the X co-ordinate that is relative
-       *   to the left side of the canvas,
-       * then, take the clientY of the event minus
-       *   the top of the canvas rectangle,
-       *   thereby, getting the Y co-ordinate that is relative
-       *   to the top part of the canvas.
-       * As we do not need the high precision available from the above
-       * calculations, we would round off to the nearest integers.
-       */
-      const mouse = [
-        Math.round(evt.clientX - rect.left),
-        Math.round(evt.clientY - rect.top),
-      ];
+      const mouse = this.#getMouseLocation(evt);
       this.path = mouse;
       this.isDrawing = true;
+      console.log(mouse);
+    };
+    this.canvas.onmousemove = (evt) => {
+      if (this.isDrawing) {
+        const mouse = this.#getMouseLocation(evt);
+        this.path.push(mouse);
+        console.log(this.path[this.path.length - 1]);
+        this.#redraw();
+      }
+    };
+    this.canvas.onmouseup = () => {
+      this.isDrawing = false;
     };
   }
+
+  #getMouseLocation = (evt) => {
+    const rect = this.canvas.getBoundingClientRect();
+    /**
+     * Obtain the mouse co-ordinate of the mouse by
+     * taking the clientX of the event minus
+     *   the left side of the canvas rectangle,
+     *   thereby, getting the X co-ordinate that is relative
+     *   to the left side of the canvas,
+     * then, take the clientY of the event minus
+     *   the top of the canvas rectangle,
+     *   thereby, getting the Y co-ordinate that is relative
+     *   to the top part of the canvas.
+     * As we do not need the high precision available from the above
+     * calculations, we would round off to the nearest integers.
+     */
+    return [
+      Math.round(evt.clientX - rect.left),
+      Math.round(evt.clientY - rect.top),
+    ];
+  };
+
+  #redraw = () => {
+    /**
+     * Use ctx clearRect to clear the canvas.
+     */
+    this.ctx.clearRect(
+      0, 0,               // from this offset (top left) 
+      this.canvas.width,
+      this.canvas.height  // to this offset (nottom right)
+    );
+    draw.path(this.ctx, this.path);
+  };
 }
