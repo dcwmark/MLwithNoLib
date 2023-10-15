dcc17e51aee1add8e130b6445853f24d5dc356c3
Entaro Adun <dcwmark@gmail.com>
2023-10-15 16:55:38 -0400

    utils print progress

--- common/utils.js
+++ common/utils.js
@@ -1,2 +1,19 @@
 // common/utils.js
 
+const utils = {};
+
+utils.formatPercent = (n) => `${ (n * 100).toFixed(2) }%`;
+
+utils.printProgress = (count, max) => {
+  process.stdout.clearLine();
+  process.stdout.cursorTo(0);
+  const percent = utils.formatPercent(
+    count / max
+  );
+  process.stdout.write(
+    `${count} / ${max} (${percent})`
+  )
+};
+
+if (typeof module !== 'undefined')
+  module.exports = utils;
