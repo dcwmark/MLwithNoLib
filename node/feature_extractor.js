fb4339e1fd178736030dfb1128ac48f11cc2edff
Entaro Adun <dcwmark@gmail.com>
2023-12-28 14:44:26 -0500

    features paths / points

--- node/feature_extractor.js
+++ node/feature_extractor.js
@@ -1,3 +1,35 @@
 // node/feature_extractor.js
 
+const fs = require('fs');
+
+const constants = require('../common/constants');
+const features = require('../common/features');
+
+console.log(`Extracting Features ...`);
+
+const samples = JSON.parse(
+    fs.readFileSync(constants.SAMPLES)
+);
+
+
+for (const sample of samples) {
+  const paths = JSON.parse(
+    fs.readFileSync(
+      `${constants.JSON_DIR}/${sample.id}.json`
+    )
+  );
+
+  sample.point = [
+    features.getPathCount(paths),
+    features.getPointCount(paths)
+  ];
+}
+
+const featureNames = ['Path Counts', 'Point Count'];
+fs.writeFileSync(
+  constants.FEATURES,
+  JSON.stringify({ featureNames, samples }),
+);
+
+console.log(`Done!`);
 
